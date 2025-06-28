94ccab5284e0e9b0907459452e37c7e19ac6f805
Achugwo Michael <139189514+MichaelAchugwo@users.noreply.github.com>
2025-06-28 12:50:31 +0100

    setup the next project and created a products.json file to store products being used in the application

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const metadata: Metadata = {
+  title: "Cart Manager",
+  description: "A simple cart management application",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <ToastContainer />
+        {children}
+      </body>
+    </html>
+  );
+}
